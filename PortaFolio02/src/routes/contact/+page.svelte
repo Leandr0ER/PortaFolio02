a9c1b8f7d12bfeea2a3eee3139199caf31a9a0a5
Leighton Estrada <lestradar@uni.edu.pe>
2025-03-20 12:52:54 -0300

    Initial commit

---
+++ PortaFolio02/src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<h1>Contact</h1>
+
+<svelte:head>
+  <title>Contact me</title>
+</svelte:head>
+
+<form action="mailto:foo@example.com" method="GET">
+    <label for="email">Email: <input id="email" type="email" name="text_email"> </label>
+    <label for="subject">Subject: <input id="subject" type="text" name="text_subject"> </label>
+    <label for="body">Body: <textarea id="body" name="text_message"></textarea> </label>                   
+</form>
+<button>Submit</button> 
---
+++ PortaFolio02/src/routes/resume/+page.svelte
@@ -0,0 +1,76 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<h1>Resume</h1>
+<hr>
+
+<div class="resume">
+    <section>
+        <header>INTEREST</header>
+        <ul>
+            <li>
+                <marker>CS</marker>
+            </li>
+            <li>
+                <marker>Maths</marker>
+            </li>
+            <li>
+                <marker>ML</marker>
+            </li>
+            <li>
+                <marker>RL</marker>
+            </li>
+        </ul>
+    </section>
+    <section>
+        <header>EXPERIENCE</header>
+        <ul>
+            <li>
+                <marker>Technical Assistant</marker>
+            </li>
+            <li>
+                <marker>Supervisor</marker>
+            </li>
+            <li>
+                <marker>Lecturer</marker>
+            </li>
+            <li>
+                <marker>Instructor</marker>
+            </li>
+        </ul>
+    </section>
+    <section>
+        <header>EDUCATION</header>
+        <ul>
+            <li>
+                <marker>B.Sc. Scientific Computing</marker>
+            </li>
+            <li>
+                <marker>M.Sc. Electronic Engineering</marker>
+            </li>
+            <li>
+                <marker>Ph.D. Student DS</marker>
+            </li>
+            <li>
+                <marker>-</marker>
+            </li>
+        </ul>
+    </section>
+    <section>
+        <header>EXTRACURRICULARS</header>
+        <ul>
+            <li>
+                <marker>Biking</marker>
+            </li>
+            <li>
+                <marker>Music</marker>
+            </li>
+            <li>
+                <marker>Sports</marker>
+            </li>
+            <li>
+                <marker>Sleep</marker>
+            </li>
+        </ul>
+    </section>
+</div>
